0efa4fec104188e6f21bf5176755efc10360c9f4
Bogdan Donchenko <40574816+goddamityouscrewedupagain@users.noreply.github.com>
2021-08-11 01:31:57 +0300

    MainView updated

--- src/main/java/com/bwap/weatherapp/WeatherApp/views/MainView.java
+++ src/main/java/com/bwap/weatherapp/WeatherApp/views/MainView.java
@@ -1,20 +1,78 @@
 package com.bwap.weatherapp.WeatherApp.views;
 
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.ClassResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ValoTheme;
 
+import java.util.ArrayList;
+
 @SpringUI(path = "")
 public class MainView extends UI {
 
     private VerticalLayout mainLayout;
+    private NativeSelect<String> unitSelect;
+    private TextField cityTextField;
+    private Button searchButton;
+    private HorizontalLayout dashboard;
+    private Label location;
+    private Label currentTemp;
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         mainLayout();
         setHeader();
-        setLogo();
+      //  setLogo();
+        setForm();
+    }
+
+    private void setForm() {
+        HorizontalLayout formLayout = new HorizontalLayout();
+        formLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+        formLayout.setSpacing(true);
+        formLayout.setMargin(true);
+        //Section Component
+        unitSelect = new NativeSelect<>();
+        ArrayList<String> items = new ArrayList<>();
+        items.add("C");
+        items.add("F");
+
+        unitSelect.setItems(items);
+        unitSelect.setValue(items.get(0));
+
+        formLayout.addComponent(unitSelect);
+
+        //cityTextField
+        cityTextField = new TextField();
+        cityTextField.setWidth("80%");
+        formLayout.addComponent(cityTextField);
+
+        //Search Button
+        searchButton = new Button();
+        searchButton.setIcon(VaadinIcons.SEARCH);
+        formLayout.addComponent(searchButton);
+
+        mainLayout.addComponents(formLayout);
+    }
+
+    private void DashboardTitle(){
+
+        dashboard = new HorizontalLayout();
+        dashboard.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+
+        //city location
+        location = new Label("Currently in ");
+        location.addStyleName(ValoTheme.LABEL_H2);
+        location.addStyleName(ValoTheme.LABEL_LIGHT);
+
+        //current TEMP
+        currentTemp = new Label("10C");
+        currentTemp.setStyleName(ValoTheme.LABEL_BOLD);
+        currentTemp.setStyleName(ValoTheme.LABEL_H1);
+
+        dashboard.addComponent(location, currentTemp);
     }
 
     private void setLogo() {
@@ -44,6 +102,6 @@ public class MainView extends UI {
         title.addStyleName(ValoTheme.LABEL_COLORED);
         header.addComponent(title);
 
-        mainLayout.addComponent(header);
+        mainLayout.addComponents(header);
     }
 }
