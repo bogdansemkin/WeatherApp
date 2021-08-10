2b328d9fb7027d847553c2e5d1527b84ac7cfeeb
Bogdan Donchenko <40574816+goddamityouscrewedupagain@users.noreply.github.com>
2021-08-10 20:19:18 +0300

    Update WeatherService.java

--- src/main/java/com/bwap/weatherapp/WeatherApp/controller/WeatherService.java
+++ src/main/java/com/bwap/weatherapp/WeatherApp/controller/WeatherService.java
@@ -1,4 +1,11 @@
 package com.bwap.weatherapp.WeatherApp.controller;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+
 public class WeatherService {
+    private OkHttpClient client;
+    private Response response;
+    private String CityName;
+    String unit;
 }
