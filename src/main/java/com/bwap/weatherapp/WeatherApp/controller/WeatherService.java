e7c292cc085b19526e3168cb8cfe61cfe6409afd
Bogdan Donchenko <40574816+goddamityouscrewedupagain@users.noreply.github.com>
2021-08-10 20:53:26 +0300

    updated View with header and mainLayout

--- src/main/java/com/bwap/weatherapp/WeatherApp/controller/WeatherService.java
+++ src/main/java/com/bwap/weatherapp/WeatherApp/controller/WeatherService.java
@@ -1,11 +1,66 @@
 package com.bwap.weatherapp.WeatherApp.controller;
 
+import okhttp3.Request;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import okhttp3.OkHttpClient;
 import okhttp3.Response;
+
+import java.io.IOException;
 
 public class WeatherService {
     private OkHttpClient client;
     private Response response;
     private String CityName;
     String unit;
+    private String API = "ab66038a741b914683f63e946f60249a";
+
+    public JSONObject getWeather(){
+        client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.openweathermap.org/data/2.5/weather?q="+getCityName()+"&units="+getUnit()+"&appid=ab66038a741b914683f63e946f60249a")
+    .build();
+        try {
+            response = client.newCall(request).execute();
+            return new JSONObject(response.body().string());
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public JSONArray returnWeatherArray() throws JSONException {
+        JSONArray weatherArray = getWeather().getJSONArray("weather");
+        return  weatherArray;
+    }
+
+    public JSONObject returnMain() throws JSONException {
+        JSONObject main = getWeather().getJSONObject("main");
+        return main;
+    }
+    public JSONObject returnWind() throws JSONException {
+        JSONObject wind = getWeather().getJSONObject("wind");
+        return wind;
+    }
+    public JSONObject returnSys() throws JSONException {
+        JSONObject sys = getWeather().getJSONObject("sys");
+        return sys;
+    }
+
+    public String getCityName() {
+        return CityName;
+    }
+
+    public void setCityName(String cityName) {
+        CityName = cityName;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
 }
--- src/main/java/com/bwap/weatherapp/WeatherApp/views/MainView.java
+++ src/main/java/com/bwap/weatherapp/WeatherApp/views/MainView.java
@@ -2,11 +2,36 @@ package com.bwap.weatherapp.WeatherApp.views;
 
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
+
 @SpringUI(path = "")
 public class MainView extends UI {
+
+    private VerticalLayout mainLayout;
     @Override
     protected void init(VaadinRequest vaadinRequest) {
+        mainLayout();
+        setHeader();
+    }
 
+    private void mainLayout() {
+         mainLayout = new VerticalLayout();
+        mainLayout.setWidth("100%");
+        mainLayout.setSpacing(true);
+        mainLayout.setMargin(true);
+        mainLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+        setContent(mainLayout);
+    }
+    private void setHeader(){
+        HorizontalLayout header = new HorizontalLayout();
+        header.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+        Label title = new Label("Testing Spring on Weather APP by Donchenko B");
+        title.addStyleName(ValoTheme.LABEL_H1);
+        title.addStyleName(ValoTheme.LABEL_BOLD);
+        title.addStyleName(ValoTheme.LABEL_COLORED);
+        header.addComponent(title);
+
+        mainLayout.addComponent(header);
     }
 }
